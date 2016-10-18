b32aa736b2ec41db379ba00bb76bb59447d5b2a0
Javier Ojeda <ojeda.javier94@gmail.com>
2016-10-18 15:31:14 -0500

    enable serialization with default url

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Add default URL for url generation
+  Rails.application.routes.default_url_options = {
+    host: 'localhost',
+    port: '3000'
+  }
 end
